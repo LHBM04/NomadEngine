0519742bbaea39db719fa3a9652775e8e6e0567b
ASOBIBA <bum2680@naver.com>
2025-07-16 09:04:45 +0900

    chore: Add new library
    - SDL2 추가
    - 불필요한 파일들 삭제

---
+++ Sources/Runtime/SandboxMain.cpp
@@ -0,0 +1,16 @@
+﻿#include <windows.h>
+
+INT APIENTRY wWinMain(
+	_In_ HINSTANCE,
+	_In_opt_ HINSTANCE,
+	_In_ LPWSTR,
+	_In_ INT
+)
+{
+	return MessageBox(
+		nullptr,
+		TEXT("Hello, World!"),
+		TEXT("Sandbox Engine"),
+		0
+	);
+}
