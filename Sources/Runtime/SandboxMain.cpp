807c948ec71b467fff9203ae11c90c6261524c88
ASOBIBA <bum2680@naver.com>
2025-07-16 10:57:59 +0900

    chore: Link libraries to .vcxproj

--- Sources/Runtime/SandboxMain.cpp
+++ Sources/Runtime/SandboxMain.cpp
@@ -1,16 +1,35 @@
 ﻿#include <windows.h>
+#include <GL/glew.h>
+#include <glfw3.h>
+#include <fmod.hpp>
+#include <GL/freeglut.h>
+#include <assimp/Importer.hpp>
+
+extern "C" int __argc;
+extern "C" char** __argv;
 
 INT APIENTRY wWinMain(
-	_In_ HINSTANCE,
-	_In_opt_ HINSTANCE,
-	_In_ LPWSTR,
-	_In_ INT
+    _In_ HINSTANCE,
+    _In_opt_ HINSTANCE,
+    _In_ LPWSTR,
+    _In_ INT
 )
 {
-	return MessageBox(
-		nullptr,
-		TEXT("Hello, World!"),
-		TEXT("Sandbox Engine"),
-		0
-	);
+	// OpenGL 라이브러리 링킹 테스트.
+    glutInit(&__argc, __argv);
+    glfwInit();
+
+    // Assimp 라이브러리 링킹 테스트.
+    Assimp::Importer importer;
+
+	// FMOD 라이브러리 링킹 테스트.
+    FMOD::System* fmodSystem = nullptr;
+    FMOD::System_Create(&fmodSystem);
+
+    return MessageBox(
+        nullptr,
+        TEXT("Hello, World!"),
+        TEXT("Sandbox Engine"),
+        0
+    );
 }
