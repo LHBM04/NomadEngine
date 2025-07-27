5bc9ad21d3fc3838b9b9c1bad509d9973be43654
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2025-07-28 00:41:47 +0900

    [chore]: Add external libraries

--- Sources/Runtime/Platform/RendererContextBase.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by bum26 on 2025-07-27.
-//
-
-#include "RendererContextBase.h"
--- Sources/Runtime/Platform/WindowContextBase.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by bum26 on 2025-07-27.
-//
-
-#include "WindowContextBase.h"
-
-namespace NomadEngine {
-} // NomadEngine
